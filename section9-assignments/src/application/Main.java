815926257e1446f22e26c7d7c11fe85eedc540f2
Milena Teixeira <milenateixeira648@gmail.com>
2021-01-27 00:01:40 -0300

    uploading a task about encapsulation and overloading.

---
+++ section9-assignments/src/application/Main.java
@@ -0,0 +1,52 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner input = new Scanner(System.in);
+		Account account;
+		
+		System.out.print("Enter account number: ");
+		int number = input.nextInt();
+		
+		System.out.print("Enter account holder: ");
+		String name = input.nextLine();
+		name = input.nextLine();
+		
+		System.out.print("Is there an initial deposit (y/n)? ");
+		char op = input.next().charAt(0);
+		if(op=='n') {
+			account = new Account(number, name);
+		}else {
+			System.out.print("Enter account initial deposit: ");
+			double deposit = input.nextDouble();
+			account = new Account(number, name, deposit);
+		}
+		
+		System.out.println("\nAccount data: ");
+		System.out.println(account);
+		
+		System.out.print("\nEnter a deposit value: ");
+		double value = input.nextDouble();
+		account.makeDeposit(value);
+		
+		System.out.println("Updated account data: ");
+		System.out.println(account);
+		
+		System.out.print("\nEnter a withdraw value: ");
+		value = input.nextDouble();
+		account.withdrawDeposit(value);
+		
+		System.out.println("Updated account data: ");
+		System.out.println(account);
+		
+		input.close();
+	}
+
+}
---
+++ section9-assignments/src/entities/Account.java
@@ -0,0 +1,52 @@
+package entities;
+
+public class Account {
+	private int number;
+	private String name;
+	private double deposit;
+	
+	public Account(int number, String name, double deposit) {
+		this.number = number;
+		this.name = name;
+		this.deposit = deposit;
+	}
+	
+	public Account(int number, String name) {
+		this.number = number;
+		this.name = name;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	public double getDeposit() {
+		return deposit;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void makeDeposit(double deposit) {
+		this.deposit += deposit;
+	}
+	
+	public void withdrawDeposit(double deposit) {
+		this.deposit -= deposit + 5;
+	}
+	
+	@Override
+	public String toString() {
+		return "Account "
+			 + getNumber()
+			 + ", Holder: "
+			 + getName()
+			 + ", Balance: $ "
+			 + String.format("%.2f", getDeposit());
+	}
+}
